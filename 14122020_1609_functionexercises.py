640c9dc88d451ce4afcf9f9868ab82e3eecdbf80
Olimpiaaxx <74835056+Olimpiaaxx@users.noreply.github.com>
2020-12-15 16:20:12 +0000

    Update 14122020_1609_functionexercises.py

--- 14122020_1609_functionexercises.py
+++ 14122020_1609_functionexercises.py
@@ -1,17 +1,13 @@
-def getUserInputCapitalized(message):
+#1
+def getUserInputTitle(message):
     x = input(message)
-    return x.capitalize()
+    return x.title()
 
 
-
-#ls = getUserInputCapitalized('Test 1: ')
-#print(as1)
-
-#print(getUserInputCapitalized('Whats your name?'))
-
-
+#2
 def getUserInputData():
     
+    print('Whats your date of birth?')
     a = input('Day: ')
     while int(a) < 1 or int(a) > 31:
          a = input('Day: ')
@@ -20,4 +16,51 @@ def getUserInputData():
     
     return a + '/' + b + '/' + c 
 
-print(getUserInputData())
+
+#3
+def addWordToList(word, ls):
+    ls.append(word)
+
+
+
+def getEmployeeDetails(ls):
+    
+    addWordToList(getUserInputTitle('Whats your first name?: '), ls)
+    addWordToList(getUserInputTitle('Whats your last name?: '), ls)
+    addWordToList(getUserInputData(), ls)
+    addWordToList(getUserInputTitle('How old are you?: '), ls)
+    addWordToList(getUserInputTitle('Whats your post code?: '), ls)
+    addWordToList(getUserInputTitle('Where are your from?: '), ls)
+
+
+
+
+
+#4
+def printDetails(ls):
+    print('Name: ', ls[0])
+    print('Surname: ', ls[1])
+    print('Date of Birth: ', ls[2])
+    print('Age: ', ls[3])
+    print('Post Code: ', ls[4])
+    print('Country: ', ls[5])
+
+
+details = []
+employee = []
+
+
+
+getEmployeeDetails(details)
+printDetails(details)
+getEmployeeDetails(employee)
+printDetails(employee)
+
+
+
+
+
+
+
+
+
