01855864103942f05d6164ab6dbb14e722134649
Olimpiaaxx <olimpiapater@gmail.com>
2021-01-28 17:52:49 +0000

    Update Single Number.py

--- Single Number.py
+++ Single Number.py
@@ -1,13 +1,22 @@
 class Solution(object):
     def singleNumber(self, nums):
+        single_list = []
         nums.sort()
         for i in range(len(nums)):
-            if i in nums and i == nums[:i]:
-                return nums.index(i)
-
+            if nums[i] = 0 and nums[i] == len(nums) -1:
+                pass
+            elif nums[i] == nums[i] -1 and nums[i] == nums[i] +1:
+                pass
+            else:
+                single_list.append(nums[i])
+        return single_list
+    
 # Solution
-test_list = [2, 2, 3]
+test_list = [2, 2, 3, 3, 4, 5, 5]
 
 my_solution = Solution()
 
 print(my_solution.singleNumber(test_list))
+
+
+
