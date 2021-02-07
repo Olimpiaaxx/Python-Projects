2dc229bb515aec02696af470dedd58ffab4a2752
Olimpiaaxx <olimpiapater@gmail.com>
2021-02-07 14:37:19 +0000

    Working on algorithms

---
+++ twoSum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+
+
+
+
+
+
+
+
+
+
+# Solution
+test_list = [3, 2, 4]
+target = 6
+
+my_solution = Solution()
+
+print(my_solution.twoSum(test_list))
